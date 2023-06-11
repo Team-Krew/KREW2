054485f93493bae1b51b6b9605a10cbf4ec2ac15
whereami2048 <whereami2048@gmail.com>
2023-06-11 17:15:25 +0900

    feat: 회원 정보 입력 구현, 메인 달력 타이틀, 사이드 바 유저 정보 연동

--- app/src/main/java/com/example/krew/controller/LoginActivity.kt
+++ app/src/main/java/com/example/krew/controller/LoginActivity.kt
@@ -7,8 +7,10 @@ import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.example.krew.ApplicationClass
 import com.example.krew.R
 import com.example.krew.databinding.ActivityLoginBinding
+import com.example.krew.model.User
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
@@ -16,6 +18,8 @@ import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.getValue
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -27,7 +31,9 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var launcher: ActivityResultLauncher<Intent>
     private lateinit var firebaseAuth: FirebaseAuth
-
+    private lateinit var cur_user: FirebaseUser
+    private lateinit var cur_user2 : User
+    val database = FirebaseDatabase.getInstance().getReference()
     private var email: String = ""
     private var tokenId: String? = null
 
@@ -51,16 +57,26 @@ class LoginActivity : AppCompatActivity() {
                                 firebaseAuth.signInWithCredential(credential)
                                     .addOnCompleteListener {
                                         if (firebaseAuth.currentUser != null) {
-                                            val user: FirebaseUser = firebaseAuth.currentUser!!
-                                            email = user.email.toString()
+                                            email = account.email.toString()
                                             val googleSignInToken = account.idToken ?: ""
                                             if (googleSignInToken != "") {
                                                 Log.d(TAG, "googleSignInToken : $googleSignInToken")
-                                                val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                                                intent.putExtra("user_email", email)
-                                                intent.putExtra("user_name", user.displayName.toString())
-                                                intent.putExtra("user_uid", user.uid)
-                                                startActivity(intent)
+                                                val user_email = ApplicationClass.sSharedPreferences.getString("user_email", null)
+                                                if(user_email == null){
+                                                    val intent = Intent(this@LoginActivity, ProfileActivity::class.java)
+                                                    ApplicationClass.spEditor.putString("user_email", email).apply()
+                                                    startActivity(intent)
+                                                }else{
+                                                    cur_user = firebaseAuth.currentUser!!
+                                                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                                                    database.child("User").child(cur_user.uid).get().addOnSuccessListener {
+                                                        val intent = Intent(this, MainActivity::class.java)
+                                                        cur_user2 = it.getValue<User>() as User
+                                                        intent.putExtra("cur_user", cur_user2)
+                                                        startActivity(intent)
+                                                    }
+
+                                                }
                                             } else {
                                                 Log.e(TAG, "googleSignInToken이 null")
                                             }
@@ -85,6 +101,7 @@ class LoginActivity : AppCompatActivity() {
 
                     val signInIntent: Intent = googleSignInClient.signInIntent
                     launcher.launch(signInIntent)
+                    gso.account
                 }
             }
         }
